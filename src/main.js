1a9e19ac75ea3728c97ce0cc2f05343e013c9db1
Roberto <betofc89@users.noreply.github.com>
2022-11-16 14:17:08 -0300

    atualizando dist

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp, VueElement } from "vue";
-import App from "./App.vue";
-import './registerServiceWorker'
-// import detectLanguage from "./utils/detectLanguage.js";
-
-const app = createApp(App);
-app.mount("#app");
-
-// createApp(App).mount("#app");
